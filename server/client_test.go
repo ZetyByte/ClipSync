a75afa7d21de42b11240a6f61982e876d369b27c
Pavel418 <pavel@vvv.az>
2023-06-13 16:04:49 +0400

    added read data unit test

--- server/client_test.go
+++ server/client_test.go
@@ -14,7 +14,6 @@ func TestClient_writeData(t *testing.T) {
 
 	conn := connectToWebsocket(server, t)
 	defer conn.Close()
-	client.conn = conn
 
 	pair := Client{sendMessage: make(chan string, 1)}
 	client.pair = &pair
@@ -25,4 +24,24 @@ func TestClient_writeData(t *testing.T) {
 	verifyMessage(conn, t, testMessage)
 
 	pair.sendMessage <- testMessage
+}
+
+func TestClient_readData(t *testing.T) {
+	testMessage := "test"
+
+	client := Client{sendMessage: make(chan string, 1)}
+
+	server := createMockServer(&client, t)
+	defer server.Close()
+
+	conn := connectToWebsocket(server, t)
+	defer conn.Close()
+
+	go client.readData()
+
+	writeMessage(conn, t, testMessage)
+
+	if message := <-client.sendMessage; message != testMessage {
+		t.Fatal("Expected message to be equal to testMessage, got", message)
+	}
 }
