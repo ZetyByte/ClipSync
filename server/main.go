72a20b9e08ed923418eb511ce18acc2cf59a91b0
Nail Shamsudinov <nail.shamsudinov@gmail.com>
2023-06-14 13:44:54 +0400

    Mutex explanation, change of endpoint, separate function for handler
    In main.go:
    1) Explained the use of mutex in comments
    2) Changed the endpoint of websocket from / to /ws
    3) Extracted handler function into separate function

--- server/main.go
+++ server/main.go
@@ -5,7 +5,16 @@ import (
 	"sync"
 )
 
+func getHandlerFunc(server *Server, mutex *sync.Mutex) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		handle(server, w, r, mutex)
+	}
+}
+
 func main() {
+	// Here, mutex is used to synchronize access to the map. Said map is used to store clients that connected by ID.
+	// Check "Server" struct in server\server.go for more information.
+	// Mutex itself is used in "handle" function in server\client.go to synchronize access to the map of clients.
 	var mutex sync.Mutex
 	var server Server = Server{data: make(map[string]*Client), registerID: make(chan *Client)}
 
@@ -13,9 +22,7 @@ func main() {
 
 	go server.process(flag)
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		handle(&server, w, r, &mutex)
-	})
+	http.HandleFunc("/ws", getHandlerFunc(&server, &mutex))
 
 	http.ListenAndServe(":8080", nil)
 
