08a2980044d71623f5bd7c24ff450ddd7902071a
Murphy052 <iguliyev@proton.me>
2023-06-23 14:09:38 +0400

    Create the signaling server

---
+++ server/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+// Create a new WebSocket upgrader
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		// Add any necessary CORS checks here
+		return true
+	},
+}
+
+// Handle incoming WebSocket connections
+func handleWebScoket(w http.ResponseWriter, r *http.Request){
+	// Upgrade the HTTP connection to a WebSocket connection
+    conn, err := upgrader.Upgrade(w, r, nil)
+    if err != nil {
+        log.Println("Upgrade error:", err)
+        return
+    }
+    defer conn.Close()
+
+	// Handle incoming signaling messages
+    for {
+        messageType, message, err := conn.ReadMessage()
+        if err != nil {
+            log.Println("Read error:", err)
+            break
+        }
+
+        // Process the signaling message (e.g., forward it to other connected clients)
+        log.Printf("Received message: %s", message)
+
+        // Send an acknowledgment response back to the sender if needed
+        if err = conn.WriteMessage(messageType, message); err != nil {
+            log.Println("Write error:", err)
+            break
+        }
+    }
+}
+
+func main(){
+	// Register the WebSocket handler
+	http.HandleFunc("/ws", handleWebScoket)
+
+	// Start the signaling server
+	log.Println("Signaling server started on http://localhost:8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("ListenAndServe error:", err)
+	}
+}
